b224644da1ae9eca6054eb04f2401c799d574e26
target10a <christoph.voelker@target10a.com>
2022-11-24 15:23:35 +0100

    Code cleanup and refactoring to eliminate compiler warnings.

--- src/android/MediaProvider.java
+++ src/android/MediaProvider.java
@@ -27,13 +27,14 @@ public class MediaProvider extends CordovaPlugin {
 
     private static final String TAG = "[MediaProvider]: ";
     private static final int INVALID_ACTION_ERROR_CODE = -1;
-    private static CallbackContext callback;
-    private static String uriStr;
-    public static final int READ_REQ_CODE = 0;
-    public static final String READ = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private static final int READ_REQ_CODE = 0;
+    private static final String READ = Manifest.permission.READ_EXTERNAL_STORAGE;
 
-    protected void getReadPermission(int requestCode) {
-        PermissionHelper.requestPermission(this, requestCode, READ);
+    private CallbackContext callbackContext;
+    private String uriStr;
+
+    protected void getReadPermission() {
+        PermissionHelper.requestPermission(this, MediaProvider.READ_REQ_CODE, READ);
     }
 
     public void initialize(CordovaInterface cordova, final CordovaWebView webView) {
@@ -44,13 +45,13 @@ public class MediaProvider extends CordovaPlugin {
      * Executes the request and returns PluginResult.
      *
      * @param action        The action to execute.
-     * @param args          JSONArry of arguments for the plugin.
+     * @param args          JSONArray of arguments for the plugin.
      * @param callbackContext The callback context through which to return stuff to caller.
      * @return              A PluginResult object with a status and message.
      */
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        this.callback = callbackContext;
+        this.callbackContext = callbackContext;
         this.uriStr = args.getString(0);
 
         if (action.equals("readText")) {
@@ -58,7 +59,7 @@ public class MediaProvider extends CordovaPlugin {
                 readText();
             }
             else {
-                getReadPermission(READ_REQ_CODE);
+                getReadPermission();
             }
 
             return true;
@@ -85,17 +86,17 @@ public class MediaProvider extends CordovaPlugin {
 
         String content = readFile(pvUrl, appContext);
 
-        this.callback.success(content);
+        this.callbackContext.success(content);
     }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
         for (int r : grantResults) {
             if (r == PackageManager.PERMISSION_DENIED) {
                 JSONObject resultObj = new JSONObject();
                 resultObj.put("code", 3);
                 resultObj.put("message", "Filesystem permission was denied.");
 
-                this.callback.error(resultObj);
+                this.callbackContext.error(resultObj);
                 return;
             }
         }
@@ -121,19 +122,14 @@ public class MediaProvider extends CordovaPlugin {
             ByteBuffer buff = ByteBuffer.allocate(bufferSize);
 
             while(channel.read(buff)>0)
-
             {
                 out.write(buff.array(), 0, buff.position());
                 buff.clear();
             }
-
-            String fileContent = new String(out.toByteArray(), StandardCharsets.UTF_8);
-            return fileContent;
-        }catch (IOException e) {
-            e.printStackTrace();
+            assetFileDescriptor.close();
+            return new String(out.toByteArray(), StandardCharsets.UTF_8);
         }
-        catch (final Exception e)
-        {
+        catch (IOException e) {
             e.printStackTrace();
         }
         return null;
